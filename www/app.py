90f40439bc8353624bc7a0b6c89fdc6ecf27ee31
zhengjp <18362973262@163.com>
2019-06-05 21:27:21 +0800

    web oneday

---
+++ www/app.py
@@ -0,0 +1,16 @@
+import logging; logging.basicConfig(level=logging.INFO)
+import asyncio
+from aiohttp import web
+
+## 定义服务器响应请求的的返回为 "Awesome Website"
+async def index(request):
+    return web.Response(body=b'<h1>Awesome Website</h1>', content_type='text/html')
+
+## 建立服务器应用，持续监听本地9000端口的http请求，对首页"/"进行响应
+def init():
+    app = web.Application()  # 创建实例
+    app.router.add_get('/', index)
+    web.run_app(app, host='127.0.0.1', port=9000)
+
+if __name__ == "__main__":
+    init()
